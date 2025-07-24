d53cb667414337e0e37fa03a1d5c2e034d548272
SWK3333 <asfas8048@gmail.com>
2025-07-24 20:30:49 +0900

    Update SearchBar.jsx

--- src/mobile/components/SearchBar.jsx
+++ src/mobile/components/SearchBar.jsx
@@ -3,20 +3,22 @@
 import React from 'react';
 import searchlogo from '../../assets/search.png';
 
-export default function SearchBar() {
+export default function SearchBar({ value, onChange, onSubmit }) {
   return (
     <div className="mobile-searchbar">
       <input
         className="mobile-search-input"
         type="text"
         placeholder="이름 또는 아이디로 검색해주세요"
+        value={value}
+        onChange={onChange}
       />
-      <button className="mobile-search-button">
+      <button className="mobile-search-button" onClick={onSubmit}>
         <img
-            src={searchlogo}
-            alt="search"
-            className="mobile-logo-search"
-          />
+          src={searchlogo}
+          alt="search"
+          className="mobile-logo-search"
+        />
       </button>
     </div>
   );
